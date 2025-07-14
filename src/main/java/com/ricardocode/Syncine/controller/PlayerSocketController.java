1b2a72f3549705a0630199c1bed6b72aa902c582
13668497443 <caio13483@gmail.com>
2025-07-14 16:49:48 -0300

    criar controller de player socket

---
+++ src/main/java/com/ricardocode/Syncine/controller/PlayerSocketController.java
@@ -0,0 +1,33 @@
+package com.ricardocode.Syncine.controller;
+
+import com.ricardocode.Syncine.dto.PlayerActionRequestDTO;
+import com.ricardocode.Syncine.dto.PlayerActionResponseDTO;
+import com.ricardocode.Syncine.model.Usuario;
+import com.ricardocode.Syncine.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class PlayerSocketController {
+
+    private final SimpMessagingTemplate messagingTemplate;
+    private final UsuarioService usuarioService;
+
+    @MessageMapping("/player/acao") // Cliente envia para /app/player/acao
+    public void receberAcao(@Payload PlayerActionRequestDTO acaoDTO) {
+        Usuario usuario = usuarioService.getUserById(acaoDTO.getUsuarioId());
+
+        PlayerActionResponseDTO responseDTO = new PlayerActionResponseDTO();
+        responseDTO.setTipoAcao(acaoDTO.getTipoAcao());
+        responseDTO.setTempoAtual(acaoDTO.getTempoAtual());
+        responseDTO.setUsuarioId(acaoDTO.getUsuarioId());
+        responseDTO.setUsername(usuario.getUsername());
+
+        String destino = "/topic/sessao/" + acaoDTO.getSessaoId();
+        messagingTemplate.convertAndSend(destino, responseDTO);
+    }
+}
