3e730845b6bc9a29f38ad325a0e29a0c81a2a38d
Ricardo <ricardofsilva0412@gmail.com>
2025-07-16 16:17:46 -0300

    atualizando o controller para deletar e editar review

--- src/main/java/com/ricardocode/Syncine/controller/ReviewController.java
+++ src/main/java/com/ricardocode/Syncine/controller/ReviewController.java
@@ -20,6 +20,7 @@ public class ReviewController {
         this.reviewService = reviewService;
     }
 
+    @PostMapping()
     public ResponseEntity<RetornoReviewDTO> criarReview(@RequestBody CriarReviewDTO criarReviewDTO) {
         Long userId = 1L;
         RetornoReviewDTO createdReview = reviewService.criarReview(criarReviewDTO, userId);
@@ -30,6 +31,35 @@ public class ReviewController {
     public ResponseEntity<List<RetornoReviewDTO>> getReviewsDoFilme(@PathVariable String imdbId) {
         List<RetornoReviewDTO> reviews = reviewService.getReviewPorFilme(imdbId);
         return ResponseEntity.ok(reviews);
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<RetornoReviewDTO> atualizarReview(
+            @PathVariable Long id,
+            @RequestBody CriarReviewDTO dto) {
+
+        try {
+            Long usuarioId = 1L;
+
+            RetornoReviewDTO reviewAtualizada = reviewService.atualizarReview(id, dto , usuarioId);
+            return ResponseEntity.ok(reviewAtualizada);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarReview(@PathVariable Long id) {
+
+        Long usuarioId = 1L;
+
+        try {
+            reviewService.deletarReview(id, usuarioId);
+            return ResponseEntity.noContent().build();
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
+        }
     }
 }
 
