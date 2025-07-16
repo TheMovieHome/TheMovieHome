f04949069ea15cd8ff0abae40693753a4d525e06
CaioCesar2005 <159182862+CaioCesar2005@users.noreply.github.com>
2025-07-16 19:13:28 -0300

    feat (model): sessao reconhece lista de participantes

--- src/main/java/com/ricardocode/Syncine/model/Sessao.java
+++ src/main/java/com/ricardocode/Syncine/model/Sessao.java
@@ -1,6 +1,8 @@
 package com.ricardocode.Syncine.model;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,40 +12,34 @@ import com.ricardocode.Syncine.model.enums.Visibilidade;
 @Setter
 @Entity
 @Table(name = "sessoes",
-       indexes = {
-           @Index(name = "idx_codigo_acesso", columnList = "codigoAcesso", unique = true)
-       })
+        indexes = {
+                @Index(name = "idx_codigo_acesso", columnList = "codigoAcesso", unique = true)
+        })
 public class Sessao {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // Nome da sala exibido na UI 
     @Column(nullable = false, length = 80)
     private String nome;
 
-    // Pública ou privada 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 8)
     private Visibilidade visibilidade;
 
-    // Código de 6 digitos usados para criar conta privada
     @Column(length = 6, nullable = true, unique = true)
     private String codigoAcesso;
 
-    // Carimbo de criação 
     @Column(nullable = false, updatable = false)
     private LocalDateTime criadaEm = LocalDateTime.now();
 
-    // Dono da sala 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "dono_id", nullable = false)
     private Usuario dono;
 
-//    Adicionar autenticação para identificar automaticamente o dono (sem precisar passar donoId).
-//
-//    Permitir entrar em uma sessão privada com o código (findByCodigoAcesso).
-//
-//    Paginar sessões públicas com Pageable.
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "sessao_participantes", joinColumns = @JoinColumn(name = "sessao_id"), inverseJoinColumns = @JoinColumn(name = "usuario_id")
+    )
+    private Set<Usuario> participantes = new HashSet<>();
 }
